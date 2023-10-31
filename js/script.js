7f45ed7fca8b854d002e6913fd97010f65cb27ec
JuanDavid-06 <jsalgadoromero79@correo.unicordoba.edu.co>
2023-10-31 17:35:02 -0500

    Cambio de ubicación

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 function iniciarMap(){
-    var coord = {lat: 8.943987384654239, lng: -75.43312137824735};
+    var coord = {lat: 8.944097, lng: -75.438563};
     var map = new google.maps.Map(document.getElementById('map'),{
         zoom:10,
         center: coord
