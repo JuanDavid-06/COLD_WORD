decd5ea72427c991829e2ce36ffefff8e5442c18
Orlando Rafael Alvarez Salcedo <143046293+Orlando2304@users.noreply.github.com>
2023-11-13 18:37:18 -0500

    Update geo.js
    --

--- js/geo.js
+++ js/geo.js
@@ -1,7 +1,10 @@
  
-
-function getMap(latitude,longitude){
-    let map = l.map('map').setView([8.9536,-75.4462], 10);
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
-L.marker([  8.94403,-75.43868]).addTo(map) .bindPopup('Tu estas .<br> AQUI.').openPopup();
-}
+ const map = L.map('map').setView([8.94411,-75.43870], 17);
+              
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+    
+    L.marker([8.94411,-75.43870]).addTo(map)
+        .bindPopup('COLD.<br> WORK.')
+        .openPopup();
