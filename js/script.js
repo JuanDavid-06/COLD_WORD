e78e02d59f86ccd8ad6108a38055b39647ee3d64
JuanDavid-06 <jsalgadoromero79@correo.unicordoba.edu.co>
2023-11-15 16:51:18 -0500

    cambios modal y pagina de servicios

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
-function iniciarMap(){
-    var coord = {lat: 8.944097, lng: -75.438563};
-    var map = new google.maps.Map(document.getElementById('map'),{
-        zoom:10,
-        center: coord
-    });
-    var marker = new google.maps.Marker({
-        position: coord,
-        map: map})
-}
+ 
+ const map = L.map('map').setView([8.94411,-75.43870], 17);
+              
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+    
+    L.marker([8.94411,-75.43870]).addTo(map)
+        .bindPopup('COLD WORLD')
+        .openPopup();
