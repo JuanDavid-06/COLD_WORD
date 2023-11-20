44f0cee4fb198ae811fa66789e801182f71f1056
Orlando Rafael Alvarez Salcedo <143046293+Orlando2304@users.noreply.github.com>
2023-11-20 16:36:29 -0500

    ..

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
  
- const map = L.map('map').setView([8.94411,-75.43870], 17);
+ const map = L.map('map').setView([8.94411,-75.43870], 15);
               
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
